d6963a52b29a0026138c73d097cc5b3ea45e5c99
BADFELLA CHAHAL <ku9690845@gmail.com>
2025-04-01 12:57:42 +0530

    Update core.py

--- core.py
+++ core.py
@@ -322,8 +322,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     try:
         await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
     except Exception:
-        await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
-
+        await m.reply_document(filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
     
     os.remove(filename)
 
